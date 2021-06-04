5899ffcae8298d32fabe1e04670fa6fb22f4198d
kosumin <be_0302@naver.com>
2021-06-04 20:07:13 +0900

    except human_s8, yeast_s3, yeast_s8 but faster

--- include/backtrack.h
+++ include/backtrack.h
@@ -21,21 +21,21 @@ struct weight_info {
 
 struct compare {
     bool operator() (const struct weight_info& w1, const struct weight_info& w2) {
-        if (w1.weight > w2.weight)
-            return true;
-        /*if (w1.weight != 0 && w1.candidates.size() == 1)
+        /*if (w1.weight > w2.weight)
+            return true;*/
+        if (w1.weight != 0 && w1.candidates.size() == 1)
             return false;
         else if (w2.weight != 0 && w2.candidates.size() == 1)
             return true;
         else if (w1.weight * w1.candidates.size() > w2.weight * w2.candidates.size())
-            return true;*/
-        else if (w1.weight == w2.weight)
+            return true;
+        /*else if (w1.weight == w2.weight)
         {
             if (w1.candidates.size() > w2.candidates.size())
                 return true;
             else
                 return false;
-        }
+        }*/
         else
             return false;
     }
